80604c4a238136fcfb020a4ed800e0d6b9a14093
ShOeLaYz <87807647+ShOeLaYz@users.noreply.github.com>
2022-02-01 10:29:23 +0530

    v1 test Added
    Add New Test Scripts

---
+++ script.js
@@ -0,0 +1,18 @@
+function hippo() {
+ location.replace("https://www.youtube.com/channel/UCYRhGZWRF0PT7fXNDgejrNw")
+}
+function stick() {
+    alert("Huh? What Did You Think My Another Cousin Had A Youtube Channel No!!!!! Just Check Hippo's Channel");
+}
+function mytwitter() {
+    location.replace("https://twitter.com/notshoelaze")
+}
+function adminpage() {
+    alert("IPs May Be Leaked Press 'Ok' To Continue");location.replace("adminlogin.html")
+}
+// Verifying technology support
+if (window.webkitNotifications) {
+    console.log('Your browser supports Notifications');
+  } else {
+    console.warn('Your browser does not support Notifications =(');
+  }
