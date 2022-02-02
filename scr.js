8e1cda289617c958b0eebb4974a362601a76e4b7
ShOeLaYz <87807647+ShOeLaYz@users.noreply.github.com>
2022-02-02 14:23:11 +0530

    Create scr.js

---
+++ scr.js
@@ -0,0 +1,29 @@
+function adminlogin() {
+    let text;
+    let username = prompt("Username:", "");
+    if (username == "admin" || username == "Sam Adams") {
+        text = "Hello " + username +" Welcome Back";
+    } else {
+        text = "Username: " +username+ " Was Not Found In Our Database";console.warn('Data(Search--none) --> Username Not Found =(');alert("This Username Does Not Exist");location.replace("#");
+    }
+    if (username == "admin" || username == "Sam Adams") {
+    let password = prompt("Password:")
+    if (password == "hipposarenoice"|| password == "yahboi") {
+        text = "";
+      } else {
+        text = "The Password '" + password + "' Is Invalid";console.error('Password(id=pwcheck)==> Password Incorrect!(');alert("Password Invalid , Try Again?");
+      }}
+      if (username == "hacker") {
+          alert("INTRUDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+          console.info('UserID=//bcv//chug-jug = Deleted hash=146349354428(');location.replace("");
+      }
+    document.getElementById("info").innerHTML = text;
+  }
+function showip() {
+    var x = document.getElementById("ip");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
